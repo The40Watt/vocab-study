f4be4c95350279248e3e73829111227018c28b40
The40Watt <stlennon21@gmail.com>
2025-03-04 17:05:41 +0000

    Newer version. Mastery changes.
    This file contains the changes to add mastery functionality.

--- my-site/show-data.php
+++ my-site/show-data.php
@@ -17,6 +17,14 @@
 
     CHANGE HISTORY:
 
+    02-03-25: Added clarification to select SQL for users word list, to sort by 'date' descending, showing newest words first.
+
+    02-03-25: Mastery Changes. Added new column to table, 'Mastered'. Clicking on the icon will update tb_vocab to set word to Y or N in the 
+              'is_mastered' column. The icon will change accordingly. 
+
+              Added new checkbox on the form. If ticked, it will hide 'mastered' words from the list. Unfortunately, the styles for the
+              checkbox are in the head of this file and not in the .css file.
+
 
 -->
 
@@ -66,10 +74,92 @@
   <title>Word Up: Your Words</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <link rel="stylesheet" href="css/stylin.css">
 </head>
+
+<style>
+  .container {
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-bottom: 12px;
+  cursor: pointer;
+  font-size: 18px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  color: #37596f;
+  font-family: "Rubik", sans-serif;
+  font-weight: 700;
+
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+  box-shadow: 2px 2px 0 0 black;
+
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark {
+  background-color: #2196F3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+
+}
+
+/* Style the checkmark/indicator */
+.container .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+
+}
+
+</style>
+
 <body>
 
 <header>
@@ -133,7 +223,12 @@
               ?>
           </select>
           <p></p>
-        <input style="width:100%;" type="submit" value="SUBMIT" class="btn btn--secondary">
+          <label class="container" style="font-family: 'Rubik', arial, sans-serif;">Hide Mastered Words
+        <input type="checkbox" id="hide" name="hide" value="Hide">
+      <span class="checkmark"></span></label>
+
+          <p></p>
+        <input style="width:100%;" type="submit" value="FILTER WORDS" class="btn btn--secondary">
       </form>
     </div>
   </div>
@@ -175,6 +270,26 @@
     </div>
   <?php } ?>
 
+  <!-- Notification of successful mastery.  -->
+  <?php 
+    if(isset($_GET['record-mastered'])){ 
+  ?>
+    <div class="alert success">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+      Success: Your changes have been saved.
+    </div>
+  <?php } ?>
+
+  <!-- Notification of unsuccessful mastery.  -->
+  <?php 
+  if(isset($_GET['record-not-mastered'])){ 
+  ?>
+    <div class="alert failure">
+      <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+      Warning. This update failed.
+    </div>
+  <?php } ?>
+
 
 
   <?php
@@ -191,6 +306,7 @@
         <td>NATIVE LANGUAGE</td>
         <td>Edit</td>
         <td>Delete</td>
+        <td>Mastered</td>
       </tr>
     </thead>
     <tbody>
@@ -199,22 +315,29 @@
         //set user_id so we can only see which words the logged in user has entered.
         $user_id = $_SESSION['user_id'];
 
-        
-        //php code to select from db
-        //if category is not set, then bring all words back for the user.
-        if (empty($category_desc)) {
-            $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id'";
 
-        } else {
-            $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id' and category_desc='$category_desc'";
-
+        /*
+          Logic below will decide which SQL statement to query the DB. There are a few scenarios:
+          1. On page load, so no category chosen or checkbox ticked.
+          2. Page has been loaded, user has selected no category and ticked the checkbox.
+          3. Page has been loaded, user has choosen a cateogry but not ticked the checkbox.
+          4. Page has been loaded, user has choosen a category and ticked the checkbox.
+        */
+        if (empty($category_desc) && (isset($_POST['hide']))) {
+            $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id' and is_mastered='N' ORDER BY date DESC";
+          } elseif (empty($category_desc) && (!isset($_POST['hide']))) {
+              $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id' ORDER BY date DESC";
+        } elseif (!empty($category_desc) && (isset($_POST['hide']))) {
+            $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id' and category_desc='$category_desc' and is_mastered='N' ORDER BY date DESC";
             //Call function to count rows for a category.
             $category_count = count_records_by_category($category_desc);
-
-    ?>
-
-    <?php
+        } else {
+            $sql = "SELECT * FROM `tb_vocab`WHERE user_id='$user_id' and category_desc='$category_desc' ORDER BY date DESC";
+            //Call function to count rows for a category.
+            $category_count = count_records_by_category($category_desc);
         }
+
+
 
         $run = mysqli_query($conn, $sql);
 
@@ -225,8 +348,16 @@
         <td><?php echo $row['en_text'] ?></td>
         <td><a href="edit-record.php?id=<?php echo $row['id']; ?>"><i class="fa fa-edit"></i></a></td>
         <td><a href="delete-record.php?id=<?php echo $row['id']; ?>"><i class="fa-regular fa-trash-can align-center" style="color: #ec4e32;"></i></a></td>
+        <td>
+          <a href="master-record.php?id=<?php echo $row['id']; ?>&is_mastered=<?php echo $row['is_mastered']; ?>"> <!-- Pass variables in URL -->
+            <?php if ($row['is_mastered'] === 'Y') { ?>
+              <i class="fa-regular fa-circle-check" style="color: #b0f0cf;"></i>
+            <?php } else { ?>
+              <i class="fa-regular fa-circle-xmark" style="color: #fb0909;"></i></i>
+            <?php } ?>
+          </a>
+        </td>
     </tr>
-
     <?php
 
     } //while ends here
@@ -237,6 +368,8 @@
   </table>
   <p></p></br>
 </main>  
+
+
 		<!-- Add the footer. -->
 		<?php include "include/footer.php" ?>
 </body>
